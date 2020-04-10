c89c00de289eb9a8f85d1b3268d2f8b049e135d2
Tom Flynn <tom.flynn@gmail.com>
2020-04-09 17:41:49 -0700

    Update travis-build.sh

--- .travis/travis-build.sh
+++ .travis/travis-build.sh
@@ -35,9 +35,22 @@ sudo cp -t "/usr/local/include/openvswitch/" include/*.h
 sudo find lib -name "*.h" -exec cp --parents -t "/usr/local/include/openvswitch/" {} \;
 popd
 
+git clone https://github.com/noironetworks/3rdparty-debian.git
+git clone https://github.com/jupp0r/prometheus-cpp.git
+cd prometheus-cpp \
+git checkout 9effb90b0c266316358680cbf862a8564eb2c2d4
+git submodule init \
+git submodule update \
+git apply ../3rdparty-debian/prometheus/prometheus-cpp.patch
+mkdir _build && cd _build
+cmake .. -DBUILD_SHARED_LIBS=ON &> /dev/null
+make $make_args &> /dev/null
+make install &> /dev/null
+mv /usr/local/lib64/libprometheus-cpp-* /usr/local/lib/
+
 pushd agent-ovs
 ./autogen.sh &> /dev/null
-./configure --enable-coverage &> /dev/null
+./configure --enable-coverage --enable-prometheus &> /dev/null
 make -j2
 sudo make install
 make check
